134482211d062542b21675f6450e52707d4e9ba7
JakubA03 <kuba.apost@gmail.com>
2026-01-03 15:54:53 +0100

    Fixes and error reporting functionality with status

--- Condition_Station_Proj.ino
+++ Condition_Station_Proj.ino
@@ -15,7 +15,15 @@ const int buttonPin = 6;
 const int potPin = A0;
 const int buzzerPin = 5;
 const int lightPin = A6;
-const int ledPin = 3;  // D3 obsluguje PWM (plynna jasnosc)
+const int ledPin = 3;
+
+
+const int ledSensePin = 4;
+
+
+unsigned long lastLedCheck = 0;
+const unsigned long ledCheckInterval = 1000;
+bool ledCableOkCached = true;
 
 int selected = 0;
 bool inMeasure = false;
@@ -27,17 +35,34 @@ unsigned long buttonDelay = 200;
 unsigned long lastBeepTime = 0;
 const unsigned long beepCooldown = 3000;
 
+bool beeping = false;
+unsigned long beepStart = 0;
+const unsigned long beepDuration = 2000;
+
 uint8_t currentContrast = 120;
 
 bool brightnessMoved = false;
 int lastPotRaw = 0;
 
-void beepError() {
+int LIGHT_RAW_MIN = 0;
+int LIGHT_RAW_MAX = 775;
+
+unsigned long lightBadSince = 0;
+
+void startBeep() {
   if (millis() - lastBeepTime < beepCooldown) return;
   lastBeepTime = millis();
+  beeping = true;
+  beepStart = millis();
   tone(buzzerPin, 2000);
-  delay(2000);
-  noTone(buzzerPin);
+}
+
+void updateBeep() {
+  if (!beeping) return;
+  if (millis() - beepStart >= beepDuration) {
+    beeping = false;
+    noTone(buzzerPin);
+  }
 }
 
 void printPaddedFloat(uint8_t col, uint8_t row, float val, uint8_t decimals, uint8_t width) {
@@ -75,11 +100,8 @@ void drawMenu() {
   for (int i = 0; i < 4 && (startIdx + i) < menuCount; i++) {
     int idx = startIdx + i;
     u8x8.setCursor(0, 1 + i * 2);
-    if (idx == selected) {
-      u8x8.print("> ");
-    } else {
-      u8x8.print("  ");
-    }
+    if (idx == selected) u8x8.print("> ");
+    else u8x8.print("  ");
     u8x8.print(menuItems[idx]);
   }
 }
@@ -128,18 +150,22 @@ void drawBrightnessLayout() {
   u8x8.print("JASNOSC OLED");
   u8x8.setCursor(0, 2);
   u8x8.print("Poziom:");
+  u8x8.setCursor(0, 6);
+  u8x8.print("Status: OK     ");
   brightnessMoved = false;
   lastPotRaw = analogRead(potPin);
 }
 
 void drawLightLayout() {
   u8x8.clearDisplay();
-  u8x8.setCursor(1, 0);
+  u8x8.setCursor(3, 0);
   u8x8.print("SWIATLO");
   u8x8.setCursor(0, 2);
   u8x8.print("Wartosc:");
   u8x8.setCursor(0, 4);
   u8x8.print("Poziom:");
+  u8x8.setCursor(0, 7);
+  u8x8.print("Status: ----   ");
 }
 
 void drawBrightnessBar(uint8_t percent) {
@@ -166,6 +192,53 @@ uint8_t currentPercentFromContrast() {
   return map(x, 0, 255, 0, 100);
 }
 
+bool lightSensorOk(int lightVal) {
+  if (lightVal <= 2 || lightVal >= 1021) {
+    if (lightBadSince == 0) lightBadSince = millis();
+    if (millis() - lightBadSince > 1500) return false;
+  } else {
+    lightBadSince = 0;
+  }
+  return true;
+}
+
+bool updateLightLed(int &lightValOut, int &lightPercentOut, int &ledPercentOut, int &ledPwmOut) {
+  int lightVal = analogRead(lightPin);
+
+  int lightPercent = map(lightVal, LIGHT_RAW_MIN, LIGHT_RAW_MAX, 0, 100);
+  lightPercent = constrain(lightPercent, 0, 100);
+
+  int ledPercent = 100 - lightPercent;
+  int ledBrightness = map(ledPercent, 0, 100, 0, 255);
+  ledBrightness = constrain(ledBrightness, 0, 255);
+
+  analogWrite(ledPin, ledBrightness);
+
+  lightValOut = lightVal;
+  lightPercentOut = lightPercent;
+  ledPercentOut = ledPercent;
+  ledPwmOut = ledBrightness;
+
+  return lightSensorOk(lightVal);
+}
+
+
+bool ledCableOk_D3_to_D4(int restorePwm) {
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(ledPin, HIGH);
+  delay(3);
+  int a = digitalRead(ledSensePin);
+
+  digitalWrite(ledPin, LOW);
+  delay(3);
+  int b = digitalRead(ledSensePin);
+
+  analogWrite(ledPin, restorePwm);
+
+  return (a != b);
+}
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
@@ -178,8 +251,10 @@ void setup() {
   pinMode(lightPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
+
+  pinMode(ledSensePin, INPUT);
+
   noTone(buzzerPin);
-  analogWrite(ledPin, 0);
 
   u8x8.begin();
   u8x8.setPowerSave(0);
@@ -191,7 +266,26 @@ void setup() {
 }
 
 void loop() {
+  updateBeep();
+
+  int lightVal = 0, lightPercent = 0, ledPercent = 0, ledPwm = 0;
+  bool lightOk = updateLightLed(lightVal, lightPercent, ledPercent, ledPwm);
+
   bool errorNow = false;
+
+  bool rtcOk = rtcPresent();
+  if (!rtcOk) errorNow = true;
+  else if (!rtc.isrunning()) errorNow = true;
+
+  if (!lightOk) errorNow = true;
+
+  if (millis() - lastLedCheck > ledCheckInterval) {
+    lastLedCheck = millis();
+    ledCableOkCached = ledCableOk_D3_to_D4(ledPwm);
+  }
+  if (!ledCableOkCached) errorNow = true;
+
+  if (errorNow) startBeep();
 
   bool button = digitalRead(buttonPin);
 
@@ -205,7 +299,6 @@ void loop() {
       else if (selected == 3) drawBrightnessLayout();
       else if (selected == 4) drawLightLayout();
     } else {
-      analogWrite(ledPin, 0);  // wylacz LED przy wyjsciu
       drawMenu();
     }
   }
@@ -224,11 +317,47 @@ void loop() {
     return;
   }
 
+  if (selected == 4) {
+    u8x8.setCursor(9, 2);
+    u8x8.print("      ");
+    u8x8.setCursor(9, 2);
+    u8x8.print(lightVal);
+
+    u8x8.setCursor(8, 4);
+    u8x8.print("      ");
+    u8x8.setCursor(8, 4);
+    if (lightPercent < 100) u8x8.print(' ');
+    if (lightPercent < 10) u8x8.print(' ');
+    u8x8.print(lightPercent);
+    u8x8.print("%");
+
+    clearLineFrom(5, 0);
+    clearLineFrom(6, 0);
+
+    const uint8_t barLen = 12;
+    uint8_t filled = (uint16_t)lightPercent * barLen / 100;
+
+    u8x8.setCursor(0, 5);
+    u8x8.print("[");
+    for (uint8_t i = 0; i < barLen; i++) u8x8.print(i < filled ? '#' : '-');
+    u8x8.print("]");
+
+    u8x8.setCursor(0, 7);
+    if (!ledCableOkCached) u8x8.print("Status: LED?   ");
+    else if (lightOk) u8x8.print("Status: OK     ");
+    else u8x8.print("Status: BLAD   ");
+
+    delay(200);
+    return;
+  }
+
   if (selected == 3) {
     int potRaw = analogRead(potPin);
     if (!brightnessMoved) {
-      if (abs(potRaw - lastPotRaw) >= 😎 brightnessMoved = true;
+      if (abs(potRaw - lastPotRaw) >= 8) brightnessMoved = true;
       drawBrightnessBar(currentPercentFromContrast());
+      u8x8.setCursor(0, 6);
+      u8x8.print("Status: OK     ");
       delay(50);
       return;
     }
@@ -244,42 +373,9 @@ void loop() {
     }
 
     drawBrightnessBar(percent);
+    u8x8.setCursor(0, 6);
+    u8x8.print("Status: OK     ");
     delay(50);
-    return;
-  }
-
-  if (selected == 4) {
-    int lightVal = analogRead(lightPin);
-    int lightPercent = map(lightVal, 0, 775, 0, 100);
-    lightPercent = constrain(lightPercent, 0, 100);
-
-    // LED jasniej gdy ciemniej (odwrocone)
-    int ledBrightness = map(lightVal, 0, 775, 255, 0);
-    ledBrightness = constrain(ledBrightness, 0, 255);
-    analogWrite(ledPin, ledBrightness);
-
-    u8x8.setCursor(9, 2);
-    u8x8.print("      ");
-    u8x8.setCursor(9, 2);
-    u8x8.print(lightVal);
-
-    const uint8_t barLen = 12;
-    uint8_t filled = (uint16_t)lightPercent * barLen / 100;
-
-    u8x8.setCursor(0, 5);
-    u8x8.print("[");
-    for (uint8_t i = 0; i < barLen; i++) u8x8.print(i < filled ? '#' : '-');
-    u8x8.print("]");
-
-    u8x8.setCursor(8, 4);
-    u8x8.print("      ");
-    u8x8.setCursor(8, 4);
-    if (lightPercent < 100) u8x8.print(' ');
-    if (lightPercent < 10) u8x8.print(' ');
-    u8x8.print((int)lightPercent);
-    u8x8.print("%");
-
-    delay(200);
     return;
   }
 
@@ -293,7 +389,7 @@ void loop() {
     } else {
       u8x8.setCursor(0, 6);
       u8x8.print("Status: BLAD   ");
-      errorNow = true;
+      startBeep();
     }
   }
   else if (selected == 1) {
@@ -308,7 +404,7 @@ void loop() {
       u8x8.print("BLAD");
       u8x8.setCursor(0, 4);
       u8x8.print("Status: BLAD   ");
-      errorNow = true;
+      startBeep();
     }
   }
   else if (selected == 2) {
@@ -322,12 +418,13 @@ void loop() {
       clearLineFrom(4, valueCol);
       u8x8.setCursor(valueCol, 4);
       u8x8.print("--.--.----");
-      errorNow = true;
+      startBeep();
     }
     else {
       if (!rtc.isrunning()) {
         u8x8.setCursor(0, 6);
         u8x8.print("Status: USTAW  ");
+        startBeep();
       } else {
         u8x8.setCursor(0, 6);
         u8x8.print("Status: OK     ");
@@ -348,8 +445,6 @@ void loop() {
       u8x8.print(dbuf);
     }
   }
-
-  if (errorNow) beepError();
 
   delay(1000);
 }
